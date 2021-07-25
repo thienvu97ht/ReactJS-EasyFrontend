0c351b415d90a0fed94525570b5ec0be6731b4af
thienvu97ht <thien.vu97ht123@gmail.com>
2021-07-25 22:54:21 +0700

    89. Style cho form dang ky

--- src/features/Auth/components/RegisterForm/index.jsx
+++ src/features/Auth/components/RegisterForm/index.jsx
@@ -1,18 +1,39 @@
 import { yupResolver } from '@hookform/resolvers/yup';
-import { Avatar, Typography } from '@material-ui/core';
+import { Avatar, Button, makeStyles, Typography } from '@material-ui/core';
 import { LockOutlined } from '@material-ui/icons';
-// import InputField from 'components/form-controls/InputField';
 import PropTypes from 'prop-types';
 import React from 'react';
 import { useForm } from 'react-hook-form';
 import * as yup from 'yup';
 import InputField from '../../../../components/form-controls/InputField';
 
+const useStyle = makeStyles((theme) => ({
+  root: {
+    paddingTop: theme.spacing(3),
+  },
+
+  avatar: {
+    margin: '0 auto',
+    backgroundColor: theme.palette.secondary.main,
+  },
+
+  title: {
+    textAlign: 'center',
+    margin: theme.spacing(2, 0, 2, 0),
+  },
+
+  submit: {
+    margin: theme.spacing(3, 0, 2, 0),
+  },
+}));
+
 RegisterForm.propTypes = {
   onSubmit: PropTypes.func,
 };
 
 function RegisterForm(props) {
+  const classes = useStyle();
+
   const schema = yup.object().shape({
     title: yup.string().required('Please enter title').min(5, 'Title is too short'),
   });
@@ -37,12 +58,12 @@ function RegisterForm(props) {
   };
 
   return (
-    <div>
-      <Avatar>
+    <div className={classes.root}>
+      <Avatar className={classes.avatar}>
         <LockOutlined></LockOutlined>
       </Avatar>
 
-      <Typography component="h3" variant="h5">
+      <Typography className={classes.title} component="h3" variant="h5">
         Create An Account
       </Typography>
 
@@ -51,6 +72,10 @@ function RegisterForm(props) {
         <InputField name="email" label="Emal" form={form} />
         <InputField name="password" label="Password" form={form} />
         <InputField name="retypePassword" label="Retype Password" form={form} />
+
+        <Button className={classes.submit} variant="contained" color="primary" fullWidth>
+          Create an account
+        </Button>
       </form>
     </div>
   );
