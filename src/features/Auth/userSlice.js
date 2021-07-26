7633f693ae00be4461499a8e2f0f0396d69cb40e
thienvu97ht <thien.vu97ht123@gmail.com>
2021-07-26 22:01:01 +0700

    96. Tạo async action

--- src/features/Auth/userSlice.js
+++ src/features/Auth/userSlice.js
@@ -1,4 +1,17 @@
-const { createSlice } = require('@reduxjs/toolkit');
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import userApi from 'api/userApi';
+
+const register = createAsyncThunk('uesr/register', async (payload) => {
+  // call API to register
+  const data = await userApi.register(payload);
+
+  // Save data to local storage
+  localStorage.setItem('access_token', data.jwt);
+  localStorage.setItem('user', JSON.stringify(data.user));
+
+  // Return user data
+  return data.user;
+});
 
 const userSlice = createSlice({
   name: 'uesr',
@@ -7,6 +20,11 @@ const userSlice = createSlice({
     setting: {},
   },
   reducers: {},
+  extraReducer: {
+    [register.fulfilled]: (state, action) => {
+      state.current = action.payload;
+    },
+  },
 });
 
 const { reducer } = userSlice;
