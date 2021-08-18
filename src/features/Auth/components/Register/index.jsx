33d84789b79c202f04486601603c9875951dd926
thienvu97ht <thien.vu97ht123@gmail.com>
2021-08-18 18:30:26 +0700

    102. Noti success

--- src/features/Auth/components/Register/index.jsx
+++ src/features/Auth/components/Register/index.jsx
@@ -1,13 +1,18 @@
 import { unwrapResult } from '@reduxjs/toolkit';
 import { register } from 'features/Auth/userSlice';
+import { useSnackbar } from 'notistack';
+import PropTypes from 'prop-types';
 import React from 'react';
 import { useDispatch } from 'react-redux';
 import RegisterForm from '../RegisterForm';
 
-Register.propTypes = {};
+Register.propTypes = {
+  closeDialog: PropTypes.func,
+};
 
 function Register(props) {
   const dispatch = useDispatch();
+  const { enqueueSnackbar } = useSnackbar();
 
   const handleSubmit = async (values) => {
     try {
@@ -19,8 +24,15 @@ function Register(props) {
       const resultAction = await dispatch(action);
       const user = unwrapResult(resultAction);
 
+      // close dialog
+      const { closeDialog } = props;
+      if (closeDialog) {
+        closeDialog();
+      }
+
       // do something here on register sucessfully
       console.log('New user', user);
+      enqueueSnackbar('Register successfully!!! ❤️❤️❤️', { variant: 'success' });
     } catch (error) {
       console.log('Failed to register: ', error);
     }
