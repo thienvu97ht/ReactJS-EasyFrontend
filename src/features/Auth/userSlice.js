f79a965ffa2424ad65da7ad8f541caf682edb02e
thienvu97ht <thien.vu97ht123@gmail.com>
2021-08-19 23:26:34 +0700

    112. Dang xuat

--- src/features/Auth/userSlice.js
+++ src/features/Auth/userSlice.js
@@ -28,7 +28,15 @@ const userSlice = createSlice({
     current: JSON.parse(localStorage.getItem(StorageKeys.USER)) || {},
     setting: {},
   },
-  reducers: {},
+  reducers: {
+    logout(state, action) {
+      // clear local storage
+      localStorage.removeItem(StorageKeys.USER);
+      localStorage.removeItem(StorageKeys.TOKEN);
+
+      state.current = {};
+    },
+  },
   extraReducer: {
     [register.fulfilled]: (state, action) => {
       state.current = action.payload;
@@ -40,5 +48,6 @@ const userSlice = createSlice({
   },
 });
 
-const { reducer } = userSlice;
+const { actions, reducer } = userSlice;
+export const { logout } = actions;
 export default reducer;
