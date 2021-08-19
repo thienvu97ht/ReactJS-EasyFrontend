71de01edeb49b939e477c174ba1ddb3b798b160d
thienvu97ht <thien.vu97ht123@gmail.com>
2021-08-19 20:40:18 +0700

    105. Setup API dang nhap

--- src/features/Auth/userSlice.js
+++ src/features/Auth/userSlice.js
@@ -8,7 +8,16 @@ export const register = createAsyncThunk('user/register', async (payload) => {
   localStorage.setItem('access_token', data.jwt);
   localStorage.setItem('user', JSON.stringify(data.user));
 
-  // Return user data
+  return data.user;
+});
+
+export const login = createAsyncThunk('user/login', async (payload) => {
+  const data = await userApi.login(payload);
+
+  // Save data to local storage
+  localStorage.setItem('access_token', data.jwt);
+  localStorage.setItem('user', JSON.stringify(data.user));
+
   return data.user;
 });
 
@@ -21,6 +30,10 @@ const userSlice = createSlice({
   reducers: {},
   extraReducer: {
     [register.fulfilled]: (state, action) => {
+      state.current = action.payload;
+    },
+
+    [login.fulfilled]: (state, action) => {
       state.current = action.payload;
     },
   },
