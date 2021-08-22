a0d8a90eeb617a603c7a9b56cf488e825e195a87
thienvu97ht <thien.vu97ht123@gmail.com>
2021-08-22 20:01:57 +0700

    126. Loc theo danh muc san pham

---
+++ src/features/Product/components/ProductFilters.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Box } from '@material-ui/core';
+import FilterByCategory from './Filters/FilterByCategory';
+import FilterByPrice from './Filters/FilterByPrice';
+
+ProductFilters.propTypes = {
+  filters: PropTypes.object.isRequired,
+  onChange: PropTypes.func,
+};
+
+function ProductFilters({ filters, onChange }) {
+  const handleCategoryChange = (newCategoryId) => {
+    if (!onChange) return;
+
+    const newFilters = {
+      ...filters,
+      'category.id': newCategoryId,
+    };
+    onChange(newFilters);
+  };
+
+  const handlePriceChange = () => {};
+
+  return (
+    <Box>
+      <FilterByCategory onChange={handleCategoryChange} />
+      <FilterByPrice onChange={handlePriceChange} />
+    </Box>
+  );
+}
+
+export default ProductFilters;
--- src/features/Product/pages/ListPage.jsx
+++ src/features/Product/pages/ListPage.jsx
@@ -1,7 +1,8 @@
-import { Box, Container, Grid, makeStyles, Paper, Typography } from '@material-ui/core';
+import { Box, Container, Grid, makeStyles, Paper } from '@material-ui/core';
 import { Pagination } from '@material-ui/lab';
 import productApi from 'api/productApi';
 import React, { useEffect, useState } from 'react';
+import ProductFilters from '../components/ProductFilters';
 import ProductList from '../components/ProductList';
 import ProductSkeletonList from '../components/ProductSkeletonList';
 import ProductSort from '../components/ProductSort';
@@ -68,12 +69,21 @@ function ListPage(props) {
     }));
   };
 
+  const handleFiltersChange = (newFilters) => {
+    setFilters((prevFilters) => ({
+      ...prevFilters,
+      ...newFilters,
+    }));
+  };
+
   return (
     <Box pt={4}>
       <Container>
         <Grid container spacing={1}>
           <Grid item className={classes.left}>
-            <Paper elevation={0}>Left column</Paper>
+            <Paper elevation={0}>
+              <ProductFilters onChange={handleFiltersChange} />
+            </Paper>
           </Grid>
 
           <Grid item className={classes.right}>
