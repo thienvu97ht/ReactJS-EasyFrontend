68502a4ee467afd0dc7d4b6c234a4766dbc9c13a
thienvu97ht <thien.vu97ht123@gmail.com>
2021-08-27 20:53:51 +0700

    139. Code component Product Thumbnail

--- src/features/Product/pages/DetailPage.jsx
+++ src/features/Product/pages/DetailPage.jsx
@@ -1,5 +1,6 @@
 import { Box, Container, Grid, makeStyles, Paper } from '@material-ui/core';
 import React from 'react';
+import ProductThumbnail from '../components/ProductThumbnail';
 
 DetailPage.propTypes = {};
 
@@ -26,7 +27,7 @@ function DetailPage() {
         <Paper elevation={0}>
           <Grid container>
             <Grid item className={classes.left}>
-              Thumbnail
+              <ProductThumbnail product={{}} />
             </Grid>
             <Grid item className={classes.right}>
               Product Info
