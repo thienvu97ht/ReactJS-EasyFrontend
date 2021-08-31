2edb5c0f90aa125f40efb4f2c4195649712fbd9e
thienvu97ht <thien.vu97ht123@gmail.com>
2021-09-01 00:25:18 +0700

    153. Tao actions cho cart items

--- src/features/Cart/cartSlice.js
+++ src/features/Cart/cartSlice.js
@@ -14,9 +14,37 @@ const cartSlide = createSlice({
     hideMiniCart(state) {
       state.showMiniCart = false;
     },
+
+    addToCart(state, action) {
+      // newItem = { id, product, quantity }
+      const newItem = action.payload;
+      const index = state.cartItem.findIndex((x) => x.id === newItem.id);
+
+      if (index >= 0) {
+        // San pham da co trong gio hang
+        state.cartItem[index].quantity += newItem.quantity;
+      } else {
+        // San pham chua co trong gio hang
+        state.cartItem.push(newItem);
+      }
+    },
+
+    setQuantity(state, action) {
+      const { id, quantity } = action.payload;
+      //   check if product is available in cart
+      const index = state.cartItem.findIndex((x) => x.id === id);
+      if (index >= 0) {
+        state.cartItem[index].quantity = quantity;
+      }
+    },
+
+    removeFromCart(state, action) {
+      const idNeedToRemove = action.payload;
+      state.cartItem = state.cartItem.filter((x) => x.id !== idNeedToRemove);
+    },
   },
 });
 
 const { actions, reducer } = cartSlide;
-export const { showMiniCart, hideMiniCart } = actions;
+export const { showMiniCart, hideMiniCart, addToCart, setQuantity, removeFromCart } = actions;
 export default reducer;
